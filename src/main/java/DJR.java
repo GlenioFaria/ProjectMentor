290e94932689567f53ff635adf65aa3399841256
GlenioFaria <glenio.faria@gmail.com>
2022-08-31 11:03:12 -0400

    Create a DJR class @Getter and setNumber

---
+++ src/main/java/DJR.java
@@ -0,0 +1,23 @@
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+@Getter
+public class DJR {
+
+    private Integer number;
+    private LinkedHashMap<BigDecimal,Boolean> map;
+    protected static String str;
+
+    public DJR(Integer number){
+        setNumber(number);
+    }
+
+    public void setNumber(Integer number) {
+        if (number <-18){
+            throw new RuntimeException();
+        }
+        this.number = number;
+    }
+}
